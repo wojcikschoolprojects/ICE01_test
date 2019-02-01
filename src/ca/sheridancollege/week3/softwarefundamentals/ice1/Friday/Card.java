86c37c36b3bd32a4508a5662cff1f15446bfed95
MichaelSoftware <michaelwojcik2@gmail.com>
2019-02-01 13:37:50 -0500

    First Update

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/Card.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/Card.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Michael Wojcik
+ * 991511320
+ * ICE01
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 
