0540c52b28820362d8ae52bb9730c1850378375e
MichaelSoftware <michaelwojcik2@gmail.com>
2019-02-01 13:36:44 -0500

    First Update

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -8,6 +8,10 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 public class CardTrick {
     public static void main(String[] args)
     {
+        card c1 = new Card();
+        c1.setSuit("Diamond");
+        c1.setValue(5);
+        
        
     }
     
